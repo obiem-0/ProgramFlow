c67d0494828b01f14ae602dd21131c6e5917c8f5
Obi Emuwa <emuwa.obi@gmail.com>
2023-07-13 23:21:43 +0100

    ProgramFlow

--- blocks.py
+++ blocks.py
@@ -1,4 +1,9 @@
-for i in range(1, 13):
-    print("No. {} squared is {} and cubed is {:4}".format(i, i ** 2, i ** 3))
-    print("*" * 80)
-print("DONE")
+name = input("Please enter your name: ")
+age = int(input("How old are you, {0}? ".format(name)))
+print(age)
+
+if age >= 18:
+    print("You are old enough to vote")
+else:
+    print("You are not old enough to vote")
+    print("Please come back in {0} years".format(18 - age))
