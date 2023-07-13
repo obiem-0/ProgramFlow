c715e1899c68949735cba489c2954b9f9a5f5085
Obi Emuwa <emuwa.obi@gmail.com>
2023-07-14 00:59:57 +0100

    ProgramFlow

--- sectionfourexercise.py
+++ sectionfourexercise.py
@@ -9,3 +9,15 @@ elif x < y:
 else:
     print('x equals y')
 # Exercise 2 ends
+
+# Challenge exercise
+print()
+name = input("Whats your name? ")
+age = int(input("Whats your age? "))
+
+if 18 <= age <= 30:
+    print("You're within the age range. Welcome!")
+else:
+    print("Unfortunately have to refuse you entry")
+
+print("Your name is {0} and you are {1} years old.".format(name, age))
